18f85f55be640e73805526ed296915a0aee4abd5
skyeer5 <danielpineda430@gmail.com>
2026-04-14 22:06:31 -0600

    implementación base de nodo y árbol binario de búsqueda

---
+++ Tarea 4/abb.py
@@ -0,0 +1,24 @@
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
+
+
+class ABB:
+    def __init__(self):
+        self.raiz = None
+
+    def insertar(self, valor):
+        self.raiz = self._insertar(self.raiz, valor)
+
+    def _insertar(self, nodo, valor):
+        if nodo is None:
+            return Nodo(valor)
+
+        if valor < nodo.valor:
+            nodo.izquierda = self._insertar(nodo.izquierda, valor)
+        else:
+            nodo.derecha = self._insertar(nodo.derecha, valor)
+
+        return nodo
